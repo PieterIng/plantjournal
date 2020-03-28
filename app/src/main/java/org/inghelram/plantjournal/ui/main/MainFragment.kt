d78aeb275d245e18debdb6399b840b416bde6e76
Pieter <pieter@inghelram.org>
2020-03-28 16:19:53 +0100

    rename binding

--- app/src/main/java/org/inghelram/plantjournal/ui/main/MainFragment.kt
+++ app/src/main/java/org/inghelram/plantjournal/ui/main/MainFragment.kt
@@ -8,11 +8,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import org.inghelram.plantjournal.R
-import org.inghelram.plantjournal.databinding.MainFragmentBinding
+import org.inghelram.plantjournal.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private lateinit var viewModel: MainViewModel
-    private lateinit var dataBinding: MainFragmentBinding
+    private lateinit var dataBinding: FragmentMainBinding
 
     companion object {
         fun newInstance() = MainFragment()
